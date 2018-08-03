500911885397de5126f1911fd51a3d5f4f7c63b2
bianchx <bianchx@163.com>
2018-08-03 15:18:58 +0800

    提交最近的代码

--- RESTCore/DataSource/IDataSource.cs
+++ RESTCore/DataSource/IDataSource.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace DataSource
 {
-    public interface IDataSource
+    public interface IListDataItem
     {
+        string GetPrimaryKey();
+    }
+
+    public interface IDataSource<T>
+    {
+        void Set(T data);
+
+        T Get();
+    }
+
+    public interface IListDataSource<T> : IDataSource<List<T>>
+        where T : IListDataItem
+    {
+        void Add(T data);
+
+        bool Delete(T data);
+
+        bool DaleteByPK(string pk);
+
+        bool Update(T data);
+
+        T GetByPK(string pk);
+
+        T FirstOrDefault(Func<T, bool> matching = null);
+
+        List<T> GetAll(Func<T, bool> matching = null);
+
+        List<T> Top(int count, Func<T, bool> matching = null);
     }
 }
---
+++ RESTCore/DataSource/MemoryDataSource.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSource
+{
+    public class MemoryDataSource<T> : IDataSource<T>
+    {
+        private T _Data;
+
+        public T Get()
+        {
+            return _Data;
+        }
+
+        public void Set(T data)
+        {
+            _Data = data;
+        }
+    }
+
+    public class MemoryListDataSource<T> : IListDataSource<T>
+        where T : IListDataItem
+    {
+        private List<T> _ListData;
+
+        public void Add(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DaleteByPK(string pk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FirstOrDefault(Func<T, bool> matching = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll(Func<T, bool> matching = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByPK(string pk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(List<T> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Top(int count, Func<T, bool> matching = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
